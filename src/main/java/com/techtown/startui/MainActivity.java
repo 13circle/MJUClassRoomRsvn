437762090b88ef9317527b035dfb4d626ecc90eb
Frontman-sjm <sjm8370@naver.com>
2019-09-24 14:05:39 +0900

    Start Animation
    앱을 실행했을 때 시작되는 로딩 화면입니다.

---
+++ src/main/java/com/techtown/startui/MainActivity.java
@@ -0,0 +1,24 @@
+package com.techtown.startui;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), next.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
