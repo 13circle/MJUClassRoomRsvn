47770b09d7c945a95deaa1fd08dbfc8c3af58262
wso0622 <wso0622@gmail.com>
2019-10-01 17:21:24 +0900

    First Commit

---
+++ app/src/main/java/com/example/mypage/realMyPage.java
@@ -0,0 +1,17 @@
+package com.example.mypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class realMyPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_my_page);
+    }
+
+
+}
