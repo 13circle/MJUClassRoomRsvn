bfedb657e6b5b8841f00133fd0c0e080060523e2
wso0622 <wso0622@gmail.com>
2019-10-08 01:01:45 +0900

    mypage수정, reserve추가

---
+++ app/src/main/java/com/techtown/startui/reserve.java
@@ -0,0 +1,46 @@
+package com.techtown.startui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class reserve extends AppCompatActivity {
+
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
